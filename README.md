674c718bfefc6a1c0bfb356f4837e7d9e5707bde
waqasa1 <waqasanwar1308@gmail.com>
2025-10-07 00:45:58 +0500

    initial

---
+++ README.md
@@ -0,0 +1,8 @@
+# My Developer Portfolio
+
+This is my personal developer portfolio built with React.js.
+
+## 🚀 Run Locally
+```bash
+npm install
+npm start
---
+++ src/containers/contact/Contact.js
@@ -0,0 +1,49 @@
+import React, {useContext} from "react";
+import "./Contact.scss";
+import SocialMedia from "../../components/socialMedia/SocialMedia";
+import {illustration, contactInfo} from "../../portfolio";
+import {Fade} from "react-reveal";
+import email from "../../assets/lottie/email";
+import DisplayLottie from "../../components/displayLottie/DisplayLottie";
+import StyleContext from "../../contexts/StyleContext";
+
+export default function Contact() {
+  const {isDark} = useContext(StyleContext);
+  return (
+    <Fade bottom duration={1000} distance="20px">
+      <div className="main contact-margin-top" id="contact">
+        <div className="contact-div-main">
+          <div className="contact-header">
+            <h1 className="heading contact-title">{contactInfo.title}</h1>
+            <div
+              className={
+                isDark ? "dark-mode contact-text-div" : "contact-text-div"
+              }
+            >
+              {contactInfo.number && (
+                <>
+                  <a
+                    className="contact-detail"
+                    href={"tel:" + contactInfo.number}
+                  >
+                    {contactInfo.number}
+                  </a>
+                  <br />
+                  <br />
+                </>
+              )}
+              <a
+                className="contact-detail-email"
+                href={"mailto:" + contactInfo.email_address}
+              >
+                {contactInfo.email_address}
+              </a>
+              <br />
+              <br />
+            </div>
+          </div>
+        </div>
+      </div>
+    </Fade>
+  );
+}
